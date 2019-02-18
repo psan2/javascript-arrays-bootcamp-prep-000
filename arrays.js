4d462f007f9559cb07d0356b0202fbc7bcc66af6
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-02-18 20:27:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
 
-function addElementToBeginningOfArray(array1,element) {
-    return [$`element`,...array1]
+function addElementToBeginningOfArray(array1,element1) {
+    return [element1,...array1]
 }
 
 //function destructivelyAddElementToBeginningOfArray
