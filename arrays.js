8c437f728c3260235c909d426c529a4a281dc83d
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-02-18 20:31:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,11 @@ function addElementToBeginningOfArray(array1,element1) {
 function destructivelyAddElementToBeginningOfArray(array1,element1) {
   return array1.unshift(element1)
 }
+
+function addElementToEndOfArray(array1,element1) {
+    return [...array1,element1]
+}
+
+function destructivelyAddElementToEndOfArray(array1,element1) {
+  return array1.push(element1)
+}
